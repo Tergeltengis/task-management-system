a1de9373561fad8cad4c7b1b6c1b7ff65674e08b
tergeltengis <tergeltengis77@gmail.com>
2023-09-01 21:43:29 +0800

    checking endpoint working properly

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to task management system");
 });
 
+// get tasks
+app.get("/tasks", (req, res) => {
+  console.log("working ");
+  res.send({ title: "test title", desc: "test desc" });
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
