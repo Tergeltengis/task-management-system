fb6cc4f575a4e45de3d3d9b94b748ddb9d127d83
tergeltengis <tergeltengis77@gmail.com>
2023-09-02 01:30:39 +0800

    feature:catch-all-routes-and-cors-added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
 import taskRouter from "./routes/task.routes";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 //router
 app.use("/api/task", taskRouter);
+
+app.use('*', (_req, res) => {
+  res.status(404).send('This page is not found');
+});
 
 app.get("/", (_req, res) => {
   res.send("Welcome to task management system");
